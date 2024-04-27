a13d4a390321447df0a876431f8a7f4f8f194153
seanjeyam <rohit.seanj@gmail.com>
2024-04-27 13:29:39 -0500

    dragon.js P2

--- classes/dragon.js
+++ classes/dragon.js
@@ -1,30 +1,24 @@
-
-
 class Dragon {
-    constructor(name, color){
-        this.name = name;
-        this.color = color;
-    }
+  constructor(name, color) {
+    this.name = name;
+    this.color = color;
+  }
 
-    // INSTANCE methods
-    breathesFire() {
-        return `${this.name} breathes fire everywhere! BURN!!!!`
-    }
+  // INSTANCE methods
+  breathesFire() {
+    return `${this.name} breathes fire everywhere! BURN!!!!`;
+  }
 
-    // CLASS methods
-    static getDragons(...dragons) {
-        // let dragonArr = ...dragons;
-        console.log(dragons);
+  // CLASS methods
+  static getDragons(...dragons) {
+    // let dragonArr = ...dragons;
+    console.log(dragons);
 
-        let dragonNames = dragons.map((dragon) => dragon.name);
-        console.log(dragonNames);
-        return dragonNames;
-    }
+    let dragonNames = dragons.map((dragon) => dragon.name);
+    console.log(dragonNames);
+    return dragonNames;
+  }
 }
-
-
-
-
 
 // examples
 const puff = new Dragon("Puff", "green");
@@ -35,7 +29,7 @@ const toothless = new Dragon("Toothless", "black");
 console.log(toothless);
 console.log(toothless.breathesFire());
 
-console.log(Dragon.getDragons(puff, toothless));  // [ 'Puff', 'Toothless' ]
+console.log(Dragon.getDragons(puff, toothless)); // [ 'Puff', 'Toothless' ]
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
--- classes/friendly-dragon.js
+++ classes/friendly-dragon.js
@@ -1,1 +1,38 @@
-// Your code here 
+// Your code here
+const Dragon = require("./dragon.js");
+
+let val = 0;
+class FriendlyDragon extends Dragon {
+  constructor(name, color, lifeGoals, friend) {
+    super(name, color);
+    this.lifeGoals = lifeGoals;
+    this.friend = friend;
+  }
+
+  hasLifeGoals() {
+    // let goals = lifeGoals.map((goal) => `${goal}`);
+    // return `${this.name} likes to ${goals}`;
+    // console.log(`${this.name} likes to ${lifeGoals}`);
+    console.log(`${this.name} likes to ${this.lifeGoals[0]}`);
+    console.log(`${this.name} likes to ${this.lifeGoals[1]}`);
+    console.log(`${this.name} likes to ${this.lifeGoals[2]}`);
+  }
+
+  helpsPeople() {
+    return `${this.name} helps their friend ${this.friend}`;
+  }
+}
+
+//examples
+const puff = new FriendlyDragon(
+  "Puff",
+  "green",
+  ["live by the sea", "frolick in the autumn mist", "help small children"],
+  "Jackie Paper"
+);
+console.log(puff);
+console.log(puff.breathesFire()); //Friendly dragons can still breathe fire!
+puff.hasLifeGoals();
+console.log(puff.helpsPeople());
+
+module.exports = FriendlyDragon;
