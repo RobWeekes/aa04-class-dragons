833c9b4bf6eb2ba63e3f62916657cd9491967ea5
robweekes <weekes.robert@gmail.com>
2024-04-27 10:09:22 -0700

    dragon.js Phase 1

--- classes/dragon.js
+++ classes/dragon.js
@@ -7,11 +7,35 @@ class Dragon {
     }
 
     // INSTANCE methods
-
-
+    breathesFire() {
+        return `${this.name} breathes fire everywhere! BURN!!!!`
+    }
 
     // CLASS methods
+    static getDragons(...dragons) {
+        // let dragonArr = ...dragons;
+        console.log(dragons);
+
+        let dragonNames = dragons.map((dragon) => dragon.name);
+        console.log(dragonNames);
+        return dragonNames;
+    }
 }
+
+
+
+
+
+// examples
+const puff = new Dragon("Puff", "green");
+console.log(puff);
+console.log(puff.breathesFire());
+
+const toothless = new Dragon("Toothless", "black");
+console.log(toothless);
+console.log(toothless.breathesFire());
+
+console.log(Dragon.getDragons(puff, toothless));  // [ 'Puff', 'Toothless' ]
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
